eb1f08f98cc4db001508b2190254b565490d1877
Luis-Hebendanz <luis.nixos@gmail.com>
2023-01-18 19:49:48 +0100

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      myapp = forAllSystems (system: pkgs.${system}.poetry2nix.mkPoetryApplication { projectDir = self; });
+    in
+    rec {
+      packages = forAllSystems (system: {
+        default =
+          let
+            spkgs = pkgs.${system};
+            pypkgs = pkgs.${system}.python310Packages;
+          in
+         spkgs.mkDerivation {
+          name = "cmake-sanitizers";
+          src = self;
+          phases = [];
+         }
+      });
+
+    };
+}
